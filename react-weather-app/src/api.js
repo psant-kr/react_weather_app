4df05f5f3f088c1039a3ba64a785dea64e86fd02
prashant kumar <prashantdoa1996@gmail.com>
2023-06-13 17:34:15 +0530

    search added but not working

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,11 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b673516ad5msh45776b30486998bp166ae1jsnb93aa9e4d951',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
